91b6f07c884dfb9fbc8da721bd5b213f87b28827
joemarc <joe.marc3@gmail.com>
2019-02-04 17:49:48 +0100

    First push

---
+++ app/bot/bot.rb
@@ -0,0 +1,19 @@
+# bot.rb
+require 'facebook/messenger'
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: access_token,
+  subscribed_fields: %w[feed mention name]
+)
+
+Bot.on :message do |message|
+  message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
+  message.sender      # => { 'id' => '1008372609250235' }
+  message.seq         # => 73
+  message.sent_at     # => 2016-04-22 21:30:36 +0200
+  message.text        # => 'Hello, bot!'
+  message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
+
+  message.reply(text: 'Hello, human!')
+end
---
+++ config/initializers/messenger_bot.rb
@@ -0,0 +1,5 @@
+Messenger::Bot.config do |config|
+    config.access_token = "EAAPGTjTkghcBAMUklTS2WPy5C2qZBjaVQT0rqR53oLcatqennynBaxbYrKR9net9L4WZCgCDd9YeioHfRHIUEgqJdEUA32IZAL46CqHTOGR3bSvnlObK4UenNMrAT3Wcx1eA7o8UCSUnSWjvH7N8q4Bfts48LX45FyaggY8UAZDZD"
+    config.validation_token = "ChatWaiter"
+    config.secret_token = "e0e9c35040172e5d69b19dcfadb709b9"
+end
